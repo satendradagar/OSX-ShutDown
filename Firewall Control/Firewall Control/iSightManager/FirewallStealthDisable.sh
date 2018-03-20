aba6a1b0a771fe388abbe3fefeae68db74d45535
Satendra Touch <satendradagar@gmail.com>
2018-03-20 10:24:08 +0530

    Added missing files

---
+++ Firewall Control/Firewall Control/iSightManager/FirewallStealthDisable.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+#echo $configPath
+defaults write /Library/Preferences/com.apple.alf stealthenabled -int 0
---
+++ Firewall Control/Firewall Control/iSightManager/FirewallStealthEnable.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#
+defaults write /Library/Preferences/com.apple.alf stealthenabled -int 1
